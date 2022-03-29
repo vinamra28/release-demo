8dae6d5a68042c8b7ed428e08cb17fea400876dc
vinamra28 <jvinamra776@gmail.com>
2022-03-29 23:05:18 +0530

    init repo

---
+++ src/test/java/com/vinamra/releasedemo/ReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinamra.releasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
